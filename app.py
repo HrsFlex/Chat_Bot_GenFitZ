f45a2187391bd0e23165d7b89d2ed0e478894e8f
Harsh Kumar <116896176+HrsFlex@users.noreply.github.com>
2024-10-06 12:52:42 +0530

    Modification test

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 import streamlit as st
 import os
+import base64
 import pathlib
 import textwrap
 
@@ -21,6 +22,26 @@ def to_markdown(text):
 
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center center;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+  
 
 
 
@@ -40,6 +61,11 @@ input=st.text_input("Input: ",key="input")
 
 submit=st.button("Ask the question")
 
+
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+set_background(f"{BASE_DIR}/bg.jpeg")
 
 
 if submit:
